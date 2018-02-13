e94b9fa24b6a39d03e75c68cdd1b5eb8c5684caa
George Brooks <truckeetrout@yahoo.com>
2018-02-13 09:58:13 -0800

    revise browser & pdf title, header & content

--- src/AppBundle/Controller/PDFController.php
+++ src/AppBundle/Controller/PDFController.php
@@ -19,7 +19,6 @@ use Knp\Snappy\Pdf;
 use PDFMerger;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
